bcefc6884dda21e2d66ff86b30e9e742b4c2b178
Nisseboy <nils@tjarnlunds.se>
2023-02-07 22:13:02 +0100

    asfa

--- index.js
+++ index.js
@@ -34,13 +34,21 @@ if (Object.keys(gates.custom).length == 0) {
   currentGate = new Gate("computer", ["a", "b", "c", "c"], ["a", "b", "c"]);
   currentGateName = "computer";
 
-  let gate = {
-    x: 10,
-    y: 5,
-    type: "and",
-    id: generateID()
-  };
-  currentGate.gates[gate.id] = gate;
+  for (let i = 0; i < 5; i++) {
+    let x = Math.floor(Math.random() * sizeDivisions);
+    let y = Math.floor(Math.random() * sizeDivisions);
+    let types = Object.keys(gates.all);
+    let type = types[Math.floor(Math.random() * types.length)];
+
+    let gate = {
+      x,
+      y,
+      type,
+      id: generateID()
+    }
+
+    currentGate.gates[gate.id] = gate;
+  }
 
   gates.custom[currentGateName] = currentGate;
   gates.all[currentGateName] = currentGate;
@@ -50,78 +58,164 @@ if (Object.keys(gates.custom).length == 0) {
 //Drawing the gates
 let drawnGates = {};
 
-setInterval(draw, 1000 / 60);
 function draw() {
   for (let i in currentGate.gates) {
     let gate = currentGate.gates[i];
     let gateType = gates.all[gate.type];
-    if (!drawnGates[i]) {
-      let gateElem = document.createElement("div");
+    if (drawnGates[i]) {
+      drawnGates[i].style.left = `calc(var(--unit) * ${gate.x})`;
+      drawnGates[i].style.top = `calc(var(--unit) * ${gate.y})`;
+      break;
+    }
+    let gateElem = document.createElement("div");
 
-      let inputLen = gateType.inputs.length;
-      let outputLen = gateType.outputs.length;
-      let gateHeight = Math.max(inputLen, outputLen) * 0.75;
+    let inputLen = gateType.inputs.length;
+    let outputLen = gateType.outputs.length;
+    let gateHeight = Math.max(inputLen, outputLen) * 0.75;
 
-      gateElem.className = "gate";
-      gateElem.style.left = `calc(var(--unit) * ${gate.x})`;
-      gateElem.style.top = `calc(var(--unit) * ${gate.y})`;
-      gateElem.innerText = gate.type;
-      gateElem.style.setProperty("--name-length", Math.ceil(gate.type.length / 2) * 2);
-      gateElem.style.setProperty("--num-inputs", inputLen);
-      gateElem.style.setProperty("--num-outputs", outputLen);
-      gateElem.style.backgroundColor = gateType.color;
+    gateElem.className = "gate";
+    gateElem.style.left = `calc(var(--unit) * ${gate.x})`;
+    gateElem.style.top = `calc(var(--unit) * ${gate.y})`;
+    gateElem.innerText = gate.type;
+    gateElem.style.setProperty("--name-length", Math.ceil(gate.type.length / 2) * 2);
+    gateElem.style.setProperty("--num-inputs", inputLen);
+    gateElem.style.setProperty("--num-outputs", outputLen);
+    gateElem.style.backgroundColor = gateType.color;
 
+    let plugHolderLeft = document.createElement("div");
+    plugHolderLeft.className = "plug-holder left";
+    for (let j in gateType.inputs) {
+      let plug = document.createElement("div");
+      plug.className = "plug";
 
-      let plugHolderLeft = document.createElement("div");
-      plugHolderLeft.className = "plug-holder left";
-      for (let j in gateType.inputs) {
-        let plug = document.createElement("div");
-        plug.className = "plug";
-
-        let baseY = gateHeight / inputLen * (j * 1 + 0.5);
-        let finalY;
-        if (baseY > gateHeight / 2) {
-          finalY = Math.ceil(baseY * step) / step;
-        } else {
-          finalY = Math.floor(baseY * step) / step;
-        }
-        plug.style.top = `calc(${finalY - 0.25} * var(--unit))`;
-
-        plugHolderLeft.appendChild(plug);
+      let baseY = gateHeight / inputLen * (j * 1 + 0.5);
+      let finalY;
+      if (baseY > gateHeight / 2) {
+        finalY = Math.ceil(baseY * step) / step;
+      } else {
+        finalY = Math.floor(baseY * step) / step;
       }
+      plug.style.top = `calc(${finalY - 0.25} * var(--unit))`;
 
-      let plugHolderRight = document.createElement("div");
-      plugHolderRight.className = "plug-holder right";
-      for (let j in gateType.outputs) {
-        let plug = document.createElement("div");
-        plug.className = "plug";
-        
-        let baseY = gateHeight / outputLen * (j * 1 + 0.5);
-        let finalY;
-        if (baseY > gateHeight / 2) {
-          finalY = Math.ceil(baseY * step) / step;
-        } else {
-          finalY = Math.floor(baseY * step) / step;
-        }
-        plug.style.top = `calc(${finalY - 0.25} * var(--unit))`;
+      plugHolderLeft.appendChild(plug);
+    }
 
-        plugHolderRight.appendChild(plug);
+    let plugHolderRight = document.createElement("div");
+    plugHolderRight.className = "plug-holder right";
+    for (let j in gateType.outputs) {
+      let plug = document.createElement("div");
+      plug.className = "plug";
+      
+      let baseY = gateHeight / outputLen * (j * 1 + 0.5);
+      let finalY;
+      if (baseY > gateHeight / 2) {
+        finalY = Math.ceil(baseY * step) / step;
+      } else {
+        finalY = Math.floor(baseY * step) / step;
       }
+      plug.style.top = `calc(${finalY - 0.25} * var(--unit))`;
 
-      gateElem.prepend(plugHolderLeft);
-      gateElem.appendChild(plugHolderRight);
-      mainElem.appendChild(gateElem);
+      plugHolderRight.appendChild(plug);
+    }
 
-      drawnGates[i] = gateElem;
+    gateElem.prepend(plugHolderLeft);
+    gateElem.appendChild(plugHolderRight);
+    mainElem.appendChild(gateElem);
+
+    gateElem.dataset.id = gate.id;
+
+    gateElem.addEventListener("mousedown", e => {startDrag(e, gate)});
+
+    drawnGates[i] = gateElem;
+    gate.elem = gateElem;
+  }
+  for (let i in drawnGates) {
+    if (!currentGate.gates[i]) {
+      drawnGates[i].remove();
+      delete drawnGates[i];
     }
   }
 }
 
+draw();
+
+
+//Dragging objects (They must have x, y, elem properties)
+let dragged = [];
+function startDrag(e, object) {
+  let coords = getCoords(e);
+  if (!e.ctrlKey) {
+    clearSelected();
+  }
+  object.elem.classList.add("selected");
+
+  dragged = [];
+  getSelected().forEach((elem, i) => {
+    //TODO: make this compatible with all object tpyes
+    let ob = currentGate.gates[elem.dataset.id];
+    dragged.push({
+      offset: {
+        x: ob.x - coords.x / unit,
+        y: ob.y - coords.y / unit,
+      },
+      object: ob
+    });
+  });
+
+  document.addEventListener("mousemove", whileDrag);
+  document.addEventListener("mouseup", stopDrag);
+  whileDrag(e);
+
+  e.stopPropagation();
+}
+function whileDrag(e) {
+  let coords = getCoords(e);
+  dragged.forEach((object, i) => {
+    object.object.x = Math.floor((coords.x / unit + object.offset.x) * 4) / 4;
+    object.object.y = Math.floor((coords.y / unit + object.offset.y) * 4) / 4;
+  });
+
+  draw();
+}
+function stopDrag(e) {
+  document.removeEventListener("mousemove", whileDrag);
+  document.removeEventListener("mouseup", stopDrag);
+}
+
+
 
 mainElem.addEventListener("mousemove", e => {
-  let uds = unit/step;
-  let x = Math.floor(e.offsetX / uds) * uds;
-  let y = Math.floor(e.offsetY / uds) * uds;
+  let coords = getCoords(e);
+  let {x, y} = toGrid(coords);
   document.getElementsByClassName("marker")[0].style.left = x + "px";
   document.getElementsByClassName("marker")[0].style.top = y + "px";
 });
+
+//Snaps a coordinate to nearest grid point
+function toGrid(pos) {
+  let uds = unit/step;
+  let x = Math.floor(pos.x / uds) * uds;
+  let y = Math.floor(pos.y / uds) * uds;
+
+  return {x, y};
+}
+
+//From a mouse event, returns the coordinates on the main element
+function getCoords(e) {
+  let box = mainElem.getBoundingClientRect();
+  return {
+    x: e.clientX - box.x,
+    y: e.clientY - box.y,
+  };
+}
+
+//Returns array of all selected elements
+function getSelected() {
+  return Array.from(document.getElementsByClassName("selected"));
+}
+//Removes selected class from all elements
+function clearSelected() {
+  getSelected().forEach(element => {
+    element.classList.remove("selected");
+  });
+}
